b378a3d54fb4eeff9c82718b127d707cca6cac98
«Diana» <diana-mk@ya.ru>
2022-08-04 18:05:54 +0700

    внесли изменения

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 11;
-double numberB = 2;
+﻿double numberA = new Random().Next(1, 10); //1 2 3 ... 9
+System.Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10); //1 2 3 ... 9
+System.Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
